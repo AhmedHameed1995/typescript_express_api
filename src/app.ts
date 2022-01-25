a16bc0706d998e29fbf12735ea3834753a37ca0d
Ahmed Hameed <ahmed.phpdeveloper@gmail.com>
2022-01-26 03:46:06 +0500

    Swagger Integration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import config from 'config'
 import connect from './utils/connect'
 import logger from './utils/logger'
 import createServer from './utils/server'
+import swaggerDocs from './utils/swagger';
 
 const port = config.get<number>('port')
 
@@ -11,4 +12,6 @@ app.listen(port, async () => {
     logger.info(`App is running at http://localhost:${port}`);
 
     await connect();
+
+    swaggerDocs(app, port)
 });
